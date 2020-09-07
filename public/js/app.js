361e9f042898b1432cd3b4bd50cb257afc74d4c8
Noah Learner <noahlearner@gmail.com>
2020-09-07 13:12:14 -0600

    commit second

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 console.log('Client side javascript file is loaded!');
-
+ 
 const weatherForm = document.querySelector('form');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
